5c8ebca56c3124133fdd7200f6bba0c8839494f4
unknown <mbrito31@alumnos.uaq.mx>
2024-05-28 20:38:05 -0600

    se agrego tabla y hamburguesa

--- src/app/screens/test/page.tsx
+++ src/app/screens/test/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 import BurgerMenu from "@/components/atoms/burger/page";
-
+import Table from "@/components/atoms/table/table";
 const Test: React.FC = () => {
     return (
-        <div className="">
-            <h1>Test Page</h1>
-            <BurgerMenu />
-        </div>
+       <div>
+          < BurgerMenu/>
+        <div className="container mx-auto p-8">
+          
+      <h1 className="text-2xl font-bold mb-4">Tabla de Calificaciones</h1>
+      <Table />
+    </div>
+    </div>
     );
 };
 
 export default Test;
-
-
-
--- src/components/atoms/burger/page.tsx
+++ src/components/atoms/burger/page.tsx
@@ -8,27 +8,27 @@ const BurgerMenu = () => {
   };
 
   return (
-    <div className={`w-48 text-white h-screen max-h-screen p-4 ${isOpen ? 'bg-customBlue ' : 'bg-transparent'}`}>
+    <div className={`fixed w-64 text-white h-screen max-h-screen p-4 ${isOpen ? 'bg-customBlue  bg-opacity-90 z-50' : 'bg-transparent'}`}>
       
-      <button className="flex text-red" onClick={handleToggle}>_ _ _</button>
-      
+      <button className="flex text-red" onClick={handleToggle}><img src="/burguer.svg" alt="" /></button>
       {isOpen && (
         <ul className="mt-4 space-y-2">
           <li className="p-2 flex items-center">
-            <img src="/logowallon.svg" alt="Item 1" className="mr-2" />
-            Item 1
+            <img src="/logowallon.svg" alt="Item 1" className="mr-2 pt-2 w-12 " />
+            Eventos Wallon
           </li>
           <li className="p-2 flex items-center">
-            <img src="/agenda.svg" alt="Item 2" className="mr-2" />
-            Item 2
+            <img src="/historial.svg" alt="Item 3" className="mr-2 pt-2 w-12" />
+            Historial Academico
           </li>
           <li className="p-2 flex items-center">
-            <img src="/historial.svg" alt="Item 3" className="mr-2" />
-            Item 3
+            <img src="/agenda.svg" alt="Item 2" className="mr-2 pt-2 w-12" />
+            Agendar con profesor
           </li>
+
           <li className="p-2 flex items-center">
-            <img src="/extracurriculares.svg" alt="Item 4" className="mr-2" />
-            Item 4
+            <img src="/extracurriculares.svg" alt="Item 4" className="mr-2 pt-2 w-12" />
+            Extracurriculares
           </li>
         </ul>
       )}
