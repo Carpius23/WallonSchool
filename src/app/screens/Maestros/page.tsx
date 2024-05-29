ec2fb2682a877eb046de7a4452f131dda455f739
Efrarico <erico21@alumnos.uaq.mx>
2024-05-29 13:47:43 -0600

    Se arreglo la pantalla de agendar maestro

--- src/app/screens/Maestros/page.tsx
+++ src/app/screens/Maestros/page.tsx
@@ -4,7 +4,6 @@ import ProfesorCard from '@/components/atoms/teachers-card/page';
 import Header from '@/components/atoms/Header/page';
 import BurgerMenu from '@/components/atoms/burger/page';
 
-
 const App: React.FC = () => {
   const profesores = [
     { nombre: 'Ericka del Rio Magaña', materia: 'Español' },
@@ -19,8 +18,9 @@ const App: React.FC = () => {
       <Header />
       <BurgerMenu />
       
-      <h1 className="text-3xl font-bold my-6  rounded-md p-4 text-center">Profesores</h1>
-      <div className="min-h-screen bg-gray-100 flex flex-col items-center">
+      <h1 className="text-3xl font-bold my-6 rounded-md p-4 text-center">Profesores</h1>
+      <div className="min-h-16 bg-white flex flex-col items-center">
+        <div className="max-w-7xl w-full mx-auto px-4">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {profesores.map((profesor, index) => (
               <ProfesorCard
@@ -31,6 +31,7 @@ const App: React.FC = () => {
             ))}
           </div>
         </div>
+      </div>
     </>
   );
 };
